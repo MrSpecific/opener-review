b6f91d6df8b22e5f2ebb03fe762388ddfd26192b
Will Christenson <will@wlcr.io>
2021-06-18 10:40:00 -0700

    Add document and typography

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+/* eslint class-methods-use-this: "off" */
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon2.png" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,700;1,300&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <h1>test</h1>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
